e49e29515a1d0354ea59734106a8ce2c34775d4d
Isaac Francisco Marte <marte07x@gmail.com>
2023-03-23 03:32:47 -0400

    changes made from thea lab environment

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,26 @@
+// imports react hook useContect
+// 
+import React, { useContext } from 'react';
 
+import { AppContext } from '../context/AppContext'; 
+
+const Budget = () =>{
+    
+
+    // Accepts the objext and returns the context value for budget
+    const { budget } = useContext(AppContext);
+
+    return (
+
+    // Div uses bootstrap alert class to add gray background
+    // By adding some text or hard coding value
+    <div className='alert alert-secondary'>
+        <span>Budget: ${budget}</span>
+    </div>
+    );
+
+}; 
+
+
+
+export default Budget; 
--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,1 +1,29 @@
+/* The Remaining component uses assigns reduce function
+to get a total of the cost and assigning a variable and 
+displaying the variable in jsx */
 
+import React, { useContext } from 'react'; 
+
+import{ AppContext } from '../context/AppContext'; 
+
+// when the user adds an expense the function causes 
+// the state to update which also make the other connected components
+// also update and re-render the context with the new value
+const Remaining = () =>{
+
+    const { expenses, budget } = useContext(AppContext); 
+    const totalExpenses = expenses.reduce((total,item) => {
+        return (total = total + item.cost); 
+    }, 0); 
+
+    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success'; 
+
+    return (
+        <div className={'alert ${alertType}'}>
+            <span>Remaining: ${budget-totalExpenses}</span>
+        </div>
+    );
+
+};
+
+export default Remaining; 
